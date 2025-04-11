82fe238a1e124e41b1c5d7ce4b852e733c8569e9
saurav-parmar <sauravparmar740@gmail.com>
2025-04-11 23:26:41 +0530

    Introduction to JavaScript & Installing Node.js

---
+++ JavaScript/Video-54/script.js
@@ -0,0 +1,21 @@
+alert("Hello World!");
+
+console.log("Code is running!");
+console.log("Code is also running!");
+console.log("Code is looking like a wow!");
+
+var a = prompt("Enter your number: ");
+var istrue = confirm("Are you sure you want to leave this page and blast your computer? ");
+
+if(istrue){
+    console.log("Computer is blasting now!");
+}
+
+else{
+    console.log("Computer is safe now!");
+}
+
+console.log("your number is: "+ a);
+
+document.title = "Hey Saurav!";
+document.body.style.backgroundColor = "aqua";
