21bc1128d170482130767d8ef212790da5def3cb
saurav-parmar <sauravparmar740@gmail.com>
2025-04-22 21:48:31 +0530

    Exercise 9 - Faulty Calculator

---
+++ JavaScript/Video-59/index.js
@@ -0,0 +1,12 @@
+/* Create a faulty calculator using JavaScript.
+
+This faulty calculator should have the following features:
+1. It takes two numbers as input from the user
+2. It performs wrong operations as follows:
+   + ---> -
+   * ---> + 
+   - ---> / 
+   / ---> **
+   
+   It performs wrong operations 10% of the times.
+*/
