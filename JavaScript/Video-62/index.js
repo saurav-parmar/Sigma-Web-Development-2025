0467772c511c7bdc36e7453519e053427ac5e55e
saurav-parmar <sauravparmar740@gmail.com>
2025-04-28 21:36:11 +0530

    JavaScript Exercise 10 - Bussines Name Generator

---
+++ JavaScript/Video-62/index.js
@@ -0,0 +1,19 @@
+/*Create a bussiness name genrator by combining list of adjectives and shop name and another word.
+
+Adjectives:
+1. Crazy
+2. Amazing
+3. Fire
+
+Shop Names:
+1. Engine
+2. Foods
+3. Garments
+
+// Another Words:
+1. Bros
+2. Limited
+3. Hub
+
+Note: You can not use arrays in this code, you can use functions to generate the names.
+*/
