5e0b6631eec53ad939c3699e56ddc8f987e92976
saurav-parmar <sauravparmar740@gmail.com>
2025-06-05 20:13:03 +0530

    JavaScript Exercise 13 - Dynamic Website Builder

---
+++ JavaScript/Video-73/Script.js
@@ -0,0 +1,5 @@
+function createCard(title, cName, views, monthsOld, duration, thumbnail) {
+    // Finish this function
+}
+
+createCard("Introduction to Backend | Sigma Web Dev Video #2", "CodeWithSaurav", "560000", 2, "31:22", "https://i.ytimg.com/vi/KB7GzBv5p4Q/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLDHpDiAzX3RJIFqB-cFW-sRYzUfGQ");
